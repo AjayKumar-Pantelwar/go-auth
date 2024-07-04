c5a007421f0cdaa9dd7507a321e88639240f962d
ajay.kumar <ajay.kumar@pantelwar.com>
2024-07-04 17:29:03 +0530

    feat: initial commit

---
+++ src/db/models.go
@@ -0,0 +1,7 @@
+package db
+
+type User struct {
+	Uid      int
+	Username string
+	Password string
+}
---
+++ src/middleware/auth.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"context"
+	util "go-authentication/src/utils"
+	"net/http"
+)
+
+func Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("at")
+
+		if err != nil {
+			http.Error(w, "Missing authorization token", http.StatusUnauthorized)
+			return
+		}
+
+		claims, err := util.ValidateJWT(cookie.Value)
+		if err != nil {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		// Set claims in context or request
+
+		ctx := context.WithValue(r.Context(), "user", claims.Username)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+	})
+}
